99a28a53b72f4465090e06669783cd59aaa57336
Matt Tilley <mtilley255@gmail.com>
2018-05-03 12:27:34 -0500

    3.3 Following example / trying something out.

--- SANDBOX-12.js
+++ SANDBOX-12.js
@@ -1,6 +1,13 @@
 /* LYNDA.COM - PROGRAMMING FOUNDATIONS WITH SIMON ALLARDICE */
 
 
+/* 3.3 Working with numbers */
+
+var a; 
+
+console.log(a);
+
+
 /*2.3 Requesting input
 
 var name = prompt("Enter your name: ");
