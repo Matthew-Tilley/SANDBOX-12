65fc92333023e3d52594f696092a365f89208b79
Matt Tilley <mtilley255@gmail.com>
2018-05-03 11:39:22 -0500

    Commented out previous example code.

--- SANDBOX-12.js
+++ SANDBOX-12.js
@@ -1,5 +1,10 @@
 /* LYNDA.COM - PROGRAMMING FOUNDATIONS WITH SIMON ALLARDICE */
 
-/* 2.2 Creating your first program in JavaScript */
 
-alert("Hello World!");
+
+
+
+
+/* 2.2 Creating your first program in JavaScript 
+
+alert("Hello World!");*/
